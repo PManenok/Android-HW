f2ee585feb869d2874dbfd447293daca996ff3c0
Palina Manenok <29563311+PManenok@users.noreply.github.com>
2018-09-20 15:30:13 +0300

    Homework 1 done

---
+++ app/src/main/java/by/itacademy/palina/homework/MainActivity.java
@@ -0,0 +1,14 @@
+package by.itacademy.palina.homework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
